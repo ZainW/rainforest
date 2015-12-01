58016f8118d6d762dcd385e6942af5d6ab6c91a9
ZainW <zainthegreatcb@gmail.com>
2015-12-01 09:44:49 -0500

    end of day

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,8 @@ class ProductsController < ApplicationController
   def show
   	@product = Product.find(params[:id])
   	if current_user
-      @review = @product.reviews.build
+      # @review = @product.reviews.build
+      @review = Review.new product: @product
   	end
   end
 
